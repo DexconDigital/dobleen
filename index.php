83b3c08d4f29290cfe10ce8a9cf7fd33853995b5
william solano <wdsp9898@gmail.com>
2020-07-23 16:12:10 -0500

    Primeros pasos
    Se añade el primer menu con su animacion esta sujeto a cambios

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | Dobleen</title>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+
+    <!-- Menu de vistas -->
+    <section>
+        <div class="overlay-navigation">
+            <nav role="navigation">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Demo</a></li>
+                    <li><a href="#">Tienda</a></li>
+                    <li><a href="#">Método</a></li>
+                    <li><a href="#">Caso de Estudio</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+        <section class="home">
+            <div class="open-overlay">
+                <span class="bar-top"></span>
+                <span class="bar-middle"></span>
+                <span class="bar-bottom"></span>
+            </div>
+        </section>
+        <div class="cajamenudos">
+            <i class="fas fa-th"></i>
+        </div>
+    </section>
+    <section>
+        <div class="container" style="display: none;">
+            <div class="col-12 border d-flex">
+                <div class="col-4 border">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, odio rerum, tempore sed repudiandae vitae dolor necessitatibus qui incidunt ut cum id ipsa officiis eos adipisci aut mollitia. Asperiores, quaerat.</p>
+                </div>
+                <div class="col-4 border">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, odio rerum, tempore sed repudiandae vitae dolor necessitatibus qui incidunt ut cum id ipsa officiis eos adipisci aut mollitia. Asperiores, quaerat.</p>
+                </div>
+                <div class="col-4 border">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, odio rerum, tempore sed repudiandae vitae dolor necessitatibus qui incidunt ut cum id ipsa officiis eos adipisci aut mollitia. Asperiores, quaerat.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+<script src="js/jquery.min.js"></script>
+<!-- <script src="js/bootstrap.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.ui.min.js"></script>
+<script src="js/menu.js"></script>
+<script src="js/all.js"></script>
+
+
+</html>
