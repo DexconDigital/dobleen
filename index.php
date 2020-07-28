0d9308b6e10979d2c615cb1718220f54c0fbd441
william solano <wdsp9898@gmail.com>
2020-07-28 16:18:05 -0500

    avance
    Se realiza  dos secciones nuevas  con animacion , aun no esta responsive

--- index.php
+++ index.php
@@ -97,30 +97,75 @@
     </section>
 
     <section>
-        <div class="container border mt-4 mb-5">
-            <div class="col-12 text-center border">
-                <img src="images/logo.png" style="width:60%; height:100%;" alt="">
+        <div class="container border contenedor_logo mt-4 mb-5">
+            <div class="col-12 text-center border logo">
+                <img src="images/logo.png" class="dimencion" style="width:60%; height:100%;" alt="">
             </div>
 
         </div>
     </section>
-    <section>
+    <section class="espacio_entre_seccion">
         <div class="container border mt-5">
             <div class="col-12 border d-flex contenedor_contenido_inical">
-                <div class="col-7 border segundo_cont_inicio">
+                <div class="col-lg-6 col-md-7 col-12 segundo_cont_inicio">
                     <div class="contenedor_texto_inicio">
-                       
+
                         <div class="linea_contenedor">
                             <div class="p-4">
-                                <span class="texto_inicio">Consultora Digital  Creativa.</span>
+                                <span class="texto_inicio">Consultora Digital Creativa.</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-5 border primera_imagen_inicio">
-                    <!-- <img src="images/imagen_inicio.png" alt=""> -->
+                <div class="col-lg-6 col-md-5 col-12 border primera_imagen_inicio">
                 </div>
-
+            </div>
+        </div>
+    </section>
+    <section class="border imagen_fondo espacio_entre_seccion2">
+        <div class="container p-0">
+            <img src="images/puntos.png" class="imagen_puntos" alt="">
+            <div class="col-12 border d-flex">
+                <div class="col-6 border">
+                    <div class="" style="margin-top:5rem;">
+                        <img src="images/fotobanner.png" class="tamaño_defaul_img" alt="">
+                    </div>
+                </div>
+                <div class="col-6 border espacio_entre_divs">
+                    <div class="border container_numero"><span class="border style_numero">01</span></div>
+                    <div class="border container_titulo">
+                        <h1 class="text-left tamaño_texto">Enfoque <br>único</h1>
+                    </div>
+                    <div class="border container_contenido_text">
+                        <p class="negrita">Combinamos estrategia, creatividad y tecnología para transformar negocios, conectándolos con las marcas, basados en los momentos que más importan en la vida de las personas.</p>
+                    </div>
+                    <div class="border contendor_cont_btn">
+                        <a class="container_btn negrita btn"><span class="cont_btn">Metodología</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="border">
+        <div class="border">
+            <div class="col-12 d-flex border pl-0">
+            <div class="imagen_numero"></div>
+                <div class="col-6 borer fondo_verde">
+                    <div class="contenedor_imagen">
+                        <img class="tamaño_defaul_img" src="images/team3.png" alt="">
+                    </div>
+                </div>
+                <div class="col-6 border espacio_entre_divs">
+                    <div class="border">
+                        <h1 class="text-left titulo_decrip">Objetivos <br>Comerciales</h1>
+                    </div>
+                    <div class="border container_contenido_text">
+                        <p class="negrita">Integramos a cada proyecto de transformación tus objetivos comerciales, para crear algo novedoso que impacte a tus clientes a través de innovaciones disruptivas.</p>
+                    </div>
+                    <div class="border contendor_cont_btn">
+                        <a class="container_btn negrita btn"><span class="cont_btn">Caso de Estudio</span></a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
