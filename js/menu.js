72f64a0a897e5f60f679f56c87a716253a5c9de0
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-03-19 18:51:17 -0500

    Nueva función para envió de datos en login
    Se agregó una nueva función de envió de datos mediante ajax, para mostrar los datos diatónicamente en el login

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,30 @@
-$('#toggle').click(function () {
-    $(this).toggleClass('active');
-    $('#overlay').toggleClass('open');
+$(document).ready(function () {
+    $('#toggle').click(function () {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    });
+    
+    $("#contact-form").on("submit", function (e) {
+        $(".enviar").attr('disabled', 'disabled').text("Cargando...");
+        if (!e.isDefaultPrevented()) {
+            var url = "Controller/login.php";
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $("#contact-form").serialize(),
+                success: function (data) {
+                    $(".enviar").removeAttr('disabled').text("Entrar");
+                    if(data == "error") {
+                        $("#mensaje").html('<div class="alert alert-danger" role="alert">ERROR: Usuario/Contraseña no es valido.</div>');
+                    }else if(data == 0){
+                        $("#mensaje").html('<div class="alert alert-danger" role="alert">Actuamente no posee la versión pro.</div>');   
+                    }else {
+                        $("#mensaje").remove();
+                        window.location.href='resultpro.php';
+                    }
+                }
+            });
+            return false;
+        }
+    });
 });
-
