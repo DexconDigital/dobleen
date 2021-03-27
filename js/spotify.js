a2da074919a9dc2d66df0c5732a97905757b7fab
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-03-26 20:23:08 -0500

    Ajustes en la lógica y nuevos elementos
    Se ajusto la paginación y ademas se agrego nueva lógica para mostrar un reproductor nuevo

--- js/spotify.js
+++ js/spotify.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
-    //https://anchor.fm/s/4c745d00/podcast/rss
-    var episodio = $("#episodio");
-    var epsdo = $("#epsdo");
-    var serie = $("#serie");
-    var autor = $("#autor");
-    var nombre = $("#nombre");
-    var img = $("#imagen");
-    var descr = $("#descripcion");
+    var intro = $(".intro");
+    var episodio = $(".episodio")
+    var epsdo = $(".epsdo");
+    var serie = $(".serie");
+    var autor = $(".autor");
+    var nombre = $(".nombre");
+    var img = $(".imagen");
+    var descr = $(".descripcion");
     var audio = $("#audio");
+    var seccion_audio = $("#seccion_audio");
     var sig_text = $("#sig_text");
     var ant_text = $("#ant_text");
     var siguiente = $("#siguiente");
     var anterior = $("#anterior");
+    siguiente.hide();
     anterior.hide();
 
     var feedURL = "https://anchor.fm/s/2d877670/podcast/rss";
@@ -21,15 +23,60 @@ $(document).ready(function () {
         url: json_parse + feedURL,
         'dataType': "json",
         success: function (data) {
-            var pos = 0;
-            var i = 1;
-            var imas = 2;
-            var dato = data.items[0];
+            var j = 1;
+            var episodios = $(".episodios");
+
+            for (var h = 0; h < data.items.length; h++) {
+
+                var episds = data.items[h];
+                var fila = '<div class="col-md-6">' +
+                    '<div class="row">' +
+                    '<div class="pdc-w pdc-aling-center">' +
+                    '<div class="col-md-12 d-flex justify-content-between p-3">' +
+                    '<span>PODCAST</span>' +
+                    '<span>T01-EP-<span>' + j + '</span></span>' +
+                    '</div>' +
+                    '<a class="col-md-12  d-flex justify-content-center podcst" href="#!" pos="' + h + '">' +
+                    '<img src="' + episds.thumbnail + '" class="container imagen pdc-img">' +
+                    '<div class="pdc-img-div p-3 position-absolute fm-regular">' +
+                    '<p class="p-0 m-0 font-weight-bold nombre text-left">' + episds.title + '</p>' +
+                    '</div>' +
+                    '</a>' +
+                    '<div class="col-md-12  p-3">' +
+                    '<div class="row">' +
+                    '<div class="col-6">' +
+                    '<span class="serie">' + data.feed.title + '</span>' +
+                    '</div>' +
+                    '<div class="col-6 text-right">' +
+                    '<span class="autor">@ ' + data.feed.author + '</span>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+                j++
+                episodios.append(fila);
+            }
+
+            $(".podcst").click(function (e) {
+                var act_pos = $(this).attr("pos");
+                window.localStorage.setItem("posiciones", act_pos);
+                window.localStorage.setItem("num_eps", parseInt(act_pos) + 1);
+                 window.location.href = "podcast.php";
+            });
+            
             var total = (data.items.length - (1));
-            var nuevo = (pos >= total) ? pos : pos + 1;
-            var viejo = (pos <= 0) ? pos : pos - 1;
+            var pos = (localStorage.posiciones == null || localStorage.posiciones > total) ? 0 : localStorage.posiciones;
+            var i = (localStorage.num_eps == null) ? 0 : localStorage.num_eps;
+            var dato = data.items[pos];
+           
+            var nuevo = (pos >= total) ? pos : parseInt(pos) + 1;
+            var viejo = (pos <= 0) ? pos : parseInt(pos) - 1;
+            var descripcion = dato.description.replace("<p>", "");
 
             serie.text(data.feed.title);
+            intro.text(data.feed.description);;
             epsdo.text(i);
             autor.text("@" + data.feed.author);
             img.attr('src', data.feed.image);
@@ -37,14 +84,97 @@ $(document).ready(function () {
             episodio.text(i + ". " + dato.title);
             nombre.text(dato.title);
             descr.html(dato.description);
-            audio.append('<audio class="sonido w-100" controls><source src="' + dato.enclosure.link + '" type="audio/ogg"><source src="' + dato.enclosure.link + '" type="audio/mpeg">Tu buscador no soporta el audio.</audio>');
+            audio.html('<audio id="player" preload="metadata"  src="' + dato.enclosure.link + '" ></audio>');
+            seccion_audio.html('<div class="row aud-bg-gris rounded-lg">' +
+                '<div class="col-xl-2 col-lg-3 col-md-3 col-12 p-3 d-flex justify-content-center">' +
+                '<img src="' + dato.thumbnail + '" class="img-fluid w-10-rem rounded-lg shadow" alt="...">' +
+                '</div>' +
+                '<div class="col-xl-10 col-lg-9 col-md-9 col-12 p-3">' +
+                '<h5 class="fm-regular font-weight-bold text-light mb-0 mt-3">' + dato.title + '</h5>' +
+                '<a class="fs-1-rem text-white text-left texto_descr_parrafo col-12 col-sm-8 text-truncate pl-0" href="' + data.feed.link + '" target="_blank">' + descripcion + '</a><br>' +
+                '<a class="btn btn-outline-light btn-sm fs-2 cursor-pointer text-light link font-weight-bold" href="' + dato.link + '" target="_blank">Seguir</a>' +
+                '<div class="form-row mt-4">' +
+                '<div class="col-8 col-sm-9 col-lg-10">' +
+                '<input id="dur" class="w-100 mt-4 h-5 shadow-none form-control-range "  type="range" name="rng" min="0" step="0.25" value="0">' +
+                '</div>' +
+                '<div class="col-2 col-sm-1 col-lg-1">' +
+                '<div class="text-light mt-2"><span id="duracion">0:00</span></div>' +
+                '</div>' +
+                '<div class="col-2 col-sm-2 col-lg-1">' +
+                '<div class="btn btn_play btn-light aud-text-gris shadow-none"><span id="icono_play"><i class="fas fa-play fa-xs"></i></span></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>');
+
+
             (total == 0) ? siguiente.hide(): siguiente.show();
-            sig_text.text(imas + ". " + data.items[nuevo].title);
+            (pos <= 0) ? anterior.hide(): anterior.show();
+            sig_text.text(data.items[nuevo].title);
+
+            //Reproductor
+            var player = $('#player');
+            var dur = $('#dur');
+            var icono_play = $("#icono_play");
+            var btn_play = $('.btn_play');
+
+            var duracion = $("#duracion");
+
+            dur.change(function () {
+                player[0].currentTime = dur.val();
+            });
+
+            player.on('loadedmetadata', function (e) {
+                dur.attr('max', this.duration)
+            });
+
+            btn_play.click(function () {
+                if (player[0].paused == false) {
+                    player[0].pause();
+                    icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                } else {
+                    player[0].play();
+                    icono_play.html('<i class=" fas fa-pause fa-xs"></i>');
+                }
+            });
+
+            player.on('timeupdate', function () {
+                var track_length = this.duration;
+                var secs = this.currentTime;
+                var progress = (secs / track_length) * 100;
+                var val = (this.currentTime - dur.attr('min')) / (dur.attr('max') - dur.attr('min'));
+                var percent = val * 100;
+
+                var tcMins = parseInt(secs / 60);
+                var tcSecs = parseInt(secs - (tcMins * 60));
+
+                if (tcSecs < 10) {
+                    tcSecs = '0' + tcSecs;
+                }
+
+                // Muesta el tiempo
+                duracion.html(tcMins + ':' + tcSecs);
+                dur.val(this.currentTime);
+
+                dur.css('background-image',
+                    '-webkit-gradient(linear, left top, right top, ' +
+                    'color-stop(' + percent + '%, #F1F1F1), ' +
+                    'color-stop(' + percent + '%, #B8B8B8)' +
+                    ')');
+
+                dur.css('background-image',
+                    '-moz-linear-gradient(left center, #F1F1F1 0%, #F1F1F1 ' + percent + '%, #B8B8B8 ' + percent + '%, #B8B8B8 100%)');
+            });
+
+            player.on('ended', function () {
+                icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                playing = false;
+            });
+
 
             siguiente.click(function (e) {
                 pos++;
                 i++;
-                imas++;
                 e.preventDefault();
                 var dato = data.items[pos];
                 var nuevo = (pos >= total) ? pos : pos + 1;
@@ -57,16 +187,95 @@ $(document).ready(function () {
                 img.attr('src', data.feed.image);
                 nombre.text(dato.title);
                 descr.html(dato.description);
-                audio.html('<audio class="sonido w-100" controls><source src="' + dato.enclosure.link + '" type="audio/ogg"><source src="' + dato.enclosure.link + '" type="audio/mpeg">Tu buscador no soporta el audio.</audio>');
+                audio.html('<audio id="player" preload="metadata"  src="' + dato.enclosure.link + '" ></audio>');
+                seccion_audio.html('<div class="row aud-bg-gris rounded-lg">' +
+                    '<div class="col-xl-2 col-lg-3 col-md-3 col-12 p-3 d-flex justify-content-center">' +
+                    '<img src="' + dato.thumbnail + '" class="img-fluid w-10-rem rounded-lg shadow" alt="...">' +
+                    '</div>' +
+                    '<div class="col-xl-10 col-lg-9 col-md-9 col-12 p-3">' +
+                    '<h5 class="fm-regular font-weight-bold text-light mb-0 mt-3">' + dato.title + '</h5>' +
+                    '<a class="fs-1-rem text-white text-left texto_descr_parrafo col-12 col-sm-8 text-truncate pl-0" href="' + data.feed.link + '" target="_blank">' + descripcion + '</a><br>' +
+                    '<a class="btn btn-outline-light btn-sm fs-2 cursor-pointer text-light link font-weight-bold" href="' + dato.link + '" target="_blank">Seguir</a>' +
+                    '<div class="form-row mt-4">' +
+                    '<div class="col-8 col-sm-9 col-lg-10">' +
+                    '<input id="dur" class="w-100 mt-4 h-5 shadow-none form-control-range "  type="range" name="rng" min="0" step="0.25" value="0">' +
+                    '</div>' +
+                    '<div class="col-2 col-sm-1 col-lg-1">' +
+                    '<div class="text-light mt-2"><span id="duracion">0:00</span></div>' +
+                    '</div>' +
+                    '<div class="col-2 col-sm-2 col-lg-1">' +
+                    '<div class="btn btn_play btn-light aud-text-gris shadow-none"><span id="icono_play"><i class="fas fa-play fa-xs"></i></span></div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>');
 
-                sig_text.text(imas + ". " + data.items[nuevo].title);
-                ant_text.text(pos + ". " + data.items[viejo].title);
+                sig_text.text(data.items[nuevo].title);
+                ant_text.text(data.items[viejo].title);
+
+                //Reproductor
+                var player = $('#player');
+                var dur = $('#dur');
+                var icono_play = $("#icono_play");
+                var btn_play = $('.btn_play');
+                var duracion = $("#duracion");
+
+                dur.change(function () {
+                    player[0].currentTime = dur.val();
+                });
+
+                player.on('loadedmetadata', function (e) {
+                    dur.attr('max', this.duration)
+                });
+
+                btn_play.click(function () {
+                    if (player[0].paused == false) {
+                        player[0].pause();
+                        icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                    } else {
+                        player[0].play();
+                        icono_play.html('<i class=" fas fa-pause fa-xs"></i>');
+                    }
+                });
+
+                player.on('timeupdate', function () {
+                    var track_length = this.duration;
+                    var secs = this.currentTime;
+                    var progress = (secs / track_length) * 100;
+                    var val = (this.currentTime - dur.attr('min')) / (dur.attr('max') - dur.attr('min'));
+                    var percent = val * 100;
+
+                    var tcMins = parseInt(secs / 60);
+                    var tcSecs = parseInt(secs - (tcMins * 60));
+
+                    if (tcSecs < 10) {
+                        tcSecs = '0' + tcSecs;
+                    }
+
+                    // Muesta el tiempo
+                    duracion.html(tcMins + ':' + tcSecs);
+                    dur.val(this.currentTime);
+
+                    dur.css('background-image',
+                        '-webkit-gradient(linear, left top, right top, ' +
+                        'color-stop(' + percent + '%, #F1F1F1), ' +
+                        'color-stop(' + percent + '%, #B8B8B8)' +
+                        ')');
+
+                    dur.css('background-image',
+                        '-moz-linear-gradient(left center, #F1F1F1 0%, #F1F1F1 ' + percent + '%, #B8B8B8 ' + percent + '%, #B8B8B8 100%)');
+                });
+
+                player.on('ended', function () {
+                    icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                    playing = false;
+                });
+
             });
 
             anterior.click(function (e) {
                 pos--;
                 i--;
-                imas--;
                 e.preventDefault();
                 var dato = data.items[pos];
                 var nuevo = (pos >= total) ? pos : pos + 1;
@@ -79,11 +288,94 @@ $(document).ready(function () {
                 img.attr('src', data.feed.image);
                 nombre.text(dato.title);
                 descr.html(dato.description);
-                audio.html('<audio class="sonido w-100" controls><source src="' + dato.enclosure.link + '" type="audio/ogg"><source src="' + dato.enclosure.link + '" type="audio/mpeg">Tu buscador no soporta el audio.</audio>');
+                audio.html('<audio id="player" preload="metadata"  src="' + dato.enclosure.link + '" ></audio>');
+                seccion_audio.html('<div class="row aud-bg-gris rounded-lg">' +
+                    '<div class="col-xl-2 col-lg-3 col-md-3 col-12 p-3 d-flex justify-content-center">' +
+                    '<img src="' + dato.thumbnail + '" class="img-fluid w-10-rem rounded-lg shadow" alt="...">' +
+                    '</div>' +
+                    '<div class="col-xl-10 col-lg-9 col-md-9 col-12 p-3">' +
+                    '<h5 class="fm-regular font-weight-bold text-light mb-0 mt-3">' + dato.title + '</h5>' +
+                    '<a class="fs-1-rem text-white text-left texto_descr_parrafo col-12 col-sm-8 text-truncate pl-0" href="' + data.feed.link + '" target="_blank">' + descripcion + '</a><br>' +
+                    '<a class="btn btn-outline-light btn-sm fs-2 cursor-pointer text-light link font-weight-bold" href="' + dato.link + '" target="_blank">Seguir</a>' +
+                    '<div class="form-row mt-4">' +
+                    '<div class="col-8 col-sm-9 col-lg-10">' +
+                    '<input id="dur" class="w-100 mt-4 h-5 shadow-none form-control-range "  type="range" name="rng" min="0" step="0.25" value="0">' +
+                    '</div>' +
+                    '<div class="col-2 col-sm-1 col-lg-1">' +
+                    '<div class="text-light mt-2"><span id="duracion">0:00</span></div>' +
+                    '</div>' +
+                    '<div class="col-2 col-sm-2 col-lg-1">' +
+                    '<div class="btn btn_play btn-light aud-text-gris shadow-none"><span id="icono_play"><i class="fas fa-play fa-xs"></i></span></div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>');
 
-                sig_text.text(imas + ". " + data.items[nuevo].title);
-                ant_text.text(pos + ". " + data.items[viejo].title);
+                sig_text.text(data.items[nuevo].title);
+                ant_text.text(data.items[viejo].title);
+
+                //Reproductor
+                var player = $('#player');
+                var dur = $('#dur');
+                var icono_play = $("#icono_play");
+                var btn_play = $('.btn_play');
+                var duracion = $("#duracion");
+
+                dur.change(function () {
+                    player[0].currentTime = dur.val();
+                });
+
+                player.on('loadedmetadata', function (e) {
+                    dur.attr('max', this.duration)
+                });
+
+                btn_play.click(function () {
+                    if (player[0].paused == false) {
+                        player[0].pause();
+                        icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                    } else {
+                        player[0].play();
+                        icono_play.html('<i class=" fas fa-pause fa-xs"></i>');
+                    }
+
+                });
+
+                player.on('timeupdate', function () {
+                    var track_length = this.duration;
+                    var secs = this.currentTime;
+                    var progress = (secs / track_length) * 100;
+                    var val = (this.currentTime - dur.attr('min')) / (dur.attr('max') - dur.attr('min'));
+                    var percent = val * 100;
+
+                    var tcMins = parseInt(secs / 60);
+                    var tcSecs = parseInt(secs - (tcMins * 60));
+
+                    if (tcSecs < 10) {
+                        tcSecs = '0' + tcSecs;
+                    }
+
+                    // Muesta el tiempo
+                    duracion.html(tcMins + ':' + tcSecs);
+                    dur.val(this.currentTime);
+
+                    dur.css('background-image',
+                        '-webkit-gradient(linear, left top, right top, ' +
+                        'color-stop(' + percent + '%, #F1F1F1), ' +
+                        'color-stop(' + percent + '%, #B8B8B8)' +
+                        ')');
+
+                    dur.css('background-image',
+                        '-moz-linear-gradient(left center, #F1F1F1 0%, #F1F1F1 ' + percent + '%, #B8B8B8 ' + percent + '%, #B8B8B8 100%)');
+                });
+
+                player.on('ended', function () {
+                    icono_play.html('<i class=" fas fa-play fa-xs"></i>');
+                    playing = false;
+                });
             });
+
+
         },
     });
+
 });
